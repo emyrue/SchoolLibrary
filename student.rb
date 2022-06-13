ba0fc0fe0ca0f034a7ca4c5013e2d8fdb94ada3e
emyrue <94935633+emyrue@users.noreply.github.com>
2022-06-13 13:41:18 -0600

    Fixed constructors

--- student.rb
+++ student.rb
@@ -2,7 +2,9 @@ require_relative 'person'
 
 class Student < Person
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
-    super(age: age, parent_permission: parent_permission, name: name)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
     @classroom = classroom
   end
 
--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,9 @@ require_relative 'person'
 
 class Teacher < Person
   def initialize(age, specialization, name = 'Unknown', parent_permission: true)
-    super(age: age, parent_permission: parent_permission, name: name)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
     @specialization = specialization
   end
 
