260a0c1ed23bcf02eb563a3f37d0040b937b4a41
emyrue <94935633+emyrue@users.noreply.github.com>
2022-06-20 09:56:24 -0600

    Created input class and named methods

---
+++ input.rb
@@ -0,0 +1,28 @@
+class Input
+  def age_input()
+  end
+
+  def name_input()
+  end
+
+  def permission_input()
+  end
+
+  def specialization_input()
+  end
+
+  def type_input()
+  end
+
+  def title_input()
+  end
+
+  def author_input()
+  end
+
+  def id_input()
+  end
+
+  def find_id(array)
+  end
+end
